574625d44600f0826a2577de87c99dfb32b21397
Darrin Eden <darrin.eden@gmail.com>
2013-03-21 14:10:54 -0700

    bump version

--- metadata.rb
+++ metadata.rb
@@ -1,9 +1,9 @@
-maintainer       "DNSimple, Inc."
-maintainer_email "ops@dnsimple.com"
-license          "Apache 2.0"
-description      "Installs/Configures exabgp"
+maintainer       'DNSimple, Inc.'
+maintainer_email 'ops@dnsimple.com'
+license          'Apache 2.0'
+description      'Installs/Configures exabgp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.1.0"
+version          '0.2.0'
 
 depends 'python'
 depends 'runit'
