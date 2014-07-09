84ee1efdd9978b76efefdc39502876c670bf7850
Anthony Eden <anthonyeden@gmail.com>
2014-07-08 17:27:59 -1000

    Remove license.

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,6 @@
 name             'exabgp'
 maintainer       'DNSimple, Inc.'
 maintainer_email 'ops@dnsimple.com'
-license          'Apache 2.0'
 description      'Installs/Configures exabgp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.9.0'
