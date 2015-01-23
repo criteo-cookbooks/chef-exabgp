03e2e4b212c1cb37cd0d883a204394028128bd8c
Aaron Kalin <akalin@martinisoftware.com>
2015-01-23 12:27:53 -0600

    Switch to git, 3.1.13 is not available there

--- attributes/exabgp.rb
+++ attributes/exabgp.rb
@@ -10,7 +10,7 @@ default[:exabgp][:ipv4][:anycast] = '127.0.0.1'
 default[:exabgp][:ipv6][:neighbor] = nil
 default[:exabgp][:ipv6][:anycast] = '::1'
 
-default[:exabgp][:source_version] = '3.1.13'
+default[:exabgp][:source_version] = '3.4.7'
 default[:exabgp][:bin_path] = '/usr/local/bin/exabgp'
 
 default[:exabgp][:watchdog_flag_file] = '/tmp/exabgp-announce'
--- recipes/source.rb
+++ recipes/source.rb
@@ -1,11 +1,9 @@
-include_recipe 'mercurial'
-
-mercurial '/usr/src/exabgp' do
-  repository 'https://code.google.com/p/exabgp/'
+git '/usr/src/exabgp' do
+  repository 'https://github.com/Exa-Networks/exabgp.git'
   reference node[:exabgp][:source_version]
   action :sync
 end
 
 node.set[:exabgp][:bin_path] = '/usr/src/exabgp/sbin/exabgp'
 
-include_recipe 'exabgp'
+include_recipe 'exabgp::default'
