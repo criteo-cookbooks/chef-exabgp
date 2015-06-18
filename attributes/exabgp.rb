c4e47b1a8013f7cb6c6a4c39148f25ee083a7a4e
Aaron Kalin <akalin@martinisoftware.com>
2015-06-18 15:33:23 -0500

    Convert symbol attributes to quoted string
    Resolves Foodcritic FC001

--- attributes/exabgp.rb
+++ attributes/exabgp.rb
@@ -1,18 +1,18 @@
-default[:exabgp][:local_as] = 12345
-default[:exabgp][:peer_as] = 12345
-default[:exabgp][:community] = [0]
+default['exabgp']['local_as'] = 12345
+default['exabgp']['peer_as'] = 12345
+default['exabgp']['community'] = [0]
 
-default[:exabgp][:hold_time] = 20
-default[:exabgp][:local_preference] = nil
+default['exabgp']['hold_time'] = 20
+default['exabgp']['local_preference'] = nil
 
-default[:exabgp][:ipv4][:neighbor] = '127.0.0.1'
-default[:exabgp][:ipv4][:anycast] = '127.0.0.1/32'
-default[:exabgp][:ipv4][:enable_static_route] = true
+default['exabgp']['ipv4']['neighbor'] = '127.0.0.1'
+default['exabgp']['ipv4']['anycast'] = '127.0.0.1/32'
+default['exabgp']['ipv4']['enable_static_route'] = true
 
-default[:exabgp][:ipv6][:neighbor] = nil
-default[:exabgp][:ipv6][:anycast] = '::1/128'
+default['exabgp']['ipv6']['neighbor'] = nil
+default['exabgp']['ipv6']['anycast'] = '::1/128'
 
-default[:exabgp][:source_version] = 'master'
-default[:exabgp][:bin_path] = '/usr/local/bin/exabgp'
+default['exabgp']['source_version'] = 'master'
+default['exabgp']['bin_path'] = '/usr/local/bin/exabgp'
 
-default[:exabgp][:watchdog_flag_file] = '/tmp/exabgp-announce'
+default['exabgp']['watchdog_flag_file'] = '/tmp/exabgp-announce'
--- recipes/source.rb
+++ recipes/source.rb
@@ -2,10 +2,10 @@ package 'git-core'
 
 git '/usr/src/exabgp' do
   repository 'https://github.com/Exa-Networks/exabgp.git'
-  reference node[:exabgp][:source_version]
+  reference node['exabgp']['source_version']
   action :sync
 end
 
-node.set[:exabgp][:bin_path] = '/usr/src/exabgp/sbin/exabgp'
+node.set['exabgp']['bin_path'] = '/usr/src/exabgp/sbin/exabgp'
 
 include_recipe 'exabgp::default'
