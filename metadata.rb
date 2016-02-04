fd88b166740437476c956d9a1a5630c2961e9d9b
Aaron Kalin <akalin@martinisoftware.com>
2016-02-04 11:58:58 -0600

    Correct Rubocop offenses

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ops@dnsimple.com'
 license          'Apache 2.0'
 description      'Installs/Configures exabgp'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue "0.0.1"
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 supports 'ubuntu', '>= 12.04'
 
