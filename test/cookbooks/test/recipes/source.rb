dcedc384d2cabd2b30e7db033cc3d86079cee9dd
Aaron Kalin <akalin@martinisoftware.com>
2017-08-31 15:38:57 -0500

    Setup the ExaBGP service correctly on source

--- test/cookbooks/test/recipes/source.rb
+++ test/cookbooks/test/recipes/source.rb
@@ -5,3 +5,9 @@ end
 exabgp 'default' do
   install_type :source
 end
+
+poise_service 'exabgp' do
+  provider :runit
+  command lazy { "#{node['exabgp']['bin_path']} /etc/exabgp/exabgp.conf" }
+  reload_signal 'USR1'
+end
