95158cf48479532289391912e670c8d2e11ab7a6
Aaron Kalin <akalin@martinisoftware.com>
2017-09-05 18:50:27 +0200

    Switch test verification around

--- test/smoke/package/package_test.rb
+++ test/smoke/package/package_test.rb
@@ -1,7 +1,16 @@
-describe processes(%r{/usr/local/bin/exabgp}) do
-  it { should exist }
+describe pip('exabgp') do
+  it { should be_installed }
 end
 
 describe file('/etc/exabgp/exabgp.conf') do
   it { should exist }
+end
+
+describe file('/etc/exabgp-anycast/exabgp.conf') do
+  it { should exist }
+end
+
+describe file('/etc/exabgp-template-vars/exabgp.conf') do
+  it { should exist }
+  its('content') { should include 'A test' }
 end
