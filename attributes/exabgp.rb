eedfb27cfec85b7b9f4c7bcc24094b169ebedf61
Aaron Kalin <akalin@martinisoftware.com>
2018-07-19 17:24:17 -0500

    Add copyright and deprecation note

--- attributes/exabgp.rb
+++ attributes/exabgp.rb
@@ -1,3 +1,23 @@
+#
+# Cookbook Name:: exabgp
+# Attributes:: default
+#
+# Copyright 2012-2018, DNSimple Corp.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# Note: These attributes are being removed in the 5.x version. Please see README for migrating to the 4.x series
 default['exabgp']['package_version'] = '3.4.19'
 default['exabgp']['source_url'] = 'https://github.com/Exa-Networks/exabgp.git'
 default['exabgp']['source_version'] = 'master'
