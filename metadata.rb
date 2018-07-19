c4af9bb79282e3b197cfadb6549f4d29df7f7488
Aaron Kalin <akalin@martinisoftware.com>
2018-07-19 17:24:56 -0500

    Update dependencies and chef version

--- metadata.rb
+++ metadata.rb
@@ -2,13 +2,14 @@ name             'exabgp'
 maintainer       'DNSimple Corp'
 maintainer_email 'ops@dnsimple.com'
 license          'Apache-2.0'
-description      'Installs/Configures exabgp'
+description      'Installs and configures ExaBGP. The Swiss Army Knife of Networking.'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '3.2.0'
 source_url       'https://github.com/dnsimple/chef-exabgp'
 issues_url       'https://github.com/dnsimple/chef-exabgp/issues'
 
-chef_version '>= 12.6'
+chef_version '>= 13.10'
 supports 'ubuntu', '>= 14.04'
 
 depends 'poise-python'
+depends 'runit'
