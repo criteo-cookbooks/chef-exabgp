1790536ed3c1ef4fe5acbb83a4309be8180ca87b
Aaron Kalin <akalin@martinisoftware.com>
2018-07-19 17:24:40 -0500

    Add some new helpers with tests

--- libraries/helpers.rb
+++ libraries/helpers.rb
@@ -7,5 +7,21 @@ module ExabgpCookbook
         'exabgp'
       end
     end
+
+    def exabgp_instance_name(config_name: 'exabgp')
+      if config_name == 'exabgp'
+        'exabgp'
+      else
+        "exabgp-#{config_name}"
+      end
+    end
+
+    def exabgp_config_path(config_path: '/etc/exabgp', config_name: 'exabgp')
+      if config_name == 'exabgp'
+        "#{config_path}/exabgp.conf"
+      else
+        "#{config_path}/exabgp-#{config_name}.conf"
+      end
+    end
   end
 end
--- spec/libraries/helper_spec.rb
+++ spec/libraries/helper_spec.rb
@@ -23,4 +23,42 @@ RSpec.describe ExabgpCookbook::Helpers do
       end
     end
   end
+
+  describe '#exabgp_instance_name' do
+    context 'when instance name is exabgp' do
+      it 'returns exabgp' do
+        expect(subject.exabgp_instance_name(config_name: 'exabgp')).to eq 'exabgp'
+      end
+    end
+
+    context 'when instance name is foo' do
+      it 'returns exabgp-foo' do
+        expect(subject.exabgp_instance_name(config_name: 'foo')).to eq 'exabgp-foo'
+      end
+    end
+  end
+
+  describe '#exabgp_config_path' do
+    context 'with no parameters' do
+      it 'returns a default config path' do
+        expect(subject.exabgp_config_path).to eq '/etc/exabgp/exabgp.conf'
+      end
+    end
+
+    context 'with a custom config_path and no config name' do
+      let(:custom_path) { '/etc/foo' }
+      it 'returns the custom config path' do
+        expect(subject.exabgp_config_path(config_path: custom_path)).to eq "#{custom_path}/exabgp.conf"
+      end
+    end
+
+    context 'with a custom config_path and a config name' do
+      let(:custom_path) { '/etc/foo' }
+      let(:custom_name) { 'baz' }
+
+      it 'returns the custom config path' do
+        expect(subject.exabgp_config_path(config_path: custom_path, config_name: custom_name)).to eq "#{custom_path}/exabgp-#{custom_name}.conf"
+      end
+    end
+  end
 end
